ac6d1f6a8a666df1962696ee92442353d616543a
Shantall Camacho <shantall.angelith@gmail.com>
2026-05-09 15:16:48 +0100

    Classe Localidade do trabalho.

---
+++ Project/Localidade.cpp
@@ -0,0 +1,18 @@
+//
+// Created by HQCamachoPC on 08/05/2026.
+//
+
+#include "Localidade.h"
+
+// Implementação dos métodos que declaraste no .h
+int Localidade::obterID() const {
+    return id;
+}
+
+std::string Localidade::obterNome() const {
+    return nome;
+}
+
+double Localidade::obterPreco() const {
+    return 0.0;
+}
