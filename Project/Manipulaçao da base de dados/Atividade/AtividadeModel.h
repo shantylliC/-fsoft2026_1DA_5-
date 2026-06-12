31ef01cf8100ec4581e2ecfda049d10c6f82c303
Shantall Camacho <shantall.angelith@gmail.com>
2026-06-12 21:06:51 +0100

    Classe Atividade (.h e .cpp)

---
+++ Project/Manipulaçao da base de dados/Atividade/AtividadeModel.h
@@ -0,0 +1,32 @@
+//
+// Created by HQCamachoPC on 12/06/2026.
+//
+
+#ifndef PROJETOVIAGENS_ATIVIDADEMODEL_H
+#define PROJETOVIAGENS_ATIVIDADEMODEL_H
+
+
+#include "LocalidadeModel.h"
+#include <string>
+
+class AtividadeModel : public LocalidadeModel {
+protected:
+    int cidadeAssociada;
+    double precoTicket;
+
+public:
+    AtividadeModel();
+    AtividadeModel(int _id, std::string _nome, int _cidadeAssociada, double _precoTicket);
+
+    virtual ~AtividadeModel() {}
+
+    // Getters
+    int obterIdCidade() const;
+    double obterPreco() const override; // Polimorfismo ativado!
+
+    // Lógica pura de leitura
+    void carregarDeLinha(std::string linha) override;
+};
+
+
+#endif //PROJETOVIAGENS_ATIVIDADEMODEL_H
