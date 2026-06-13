b3badfd239d7286a8dc799015db1a297f668c72f
diogoribeiro <1250871@isep.ipp.pt>
2026-06-13 14:48:15 +0100

    testes - datas

---
+++ ProjectTester/Data - Testers/cmake-build-debug/googletest-main/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/diogo/CLionProjects/-fsoft2026_1DA_5-/ProjectTester/Data - Testers/googletest-main
+# Build directory: C:/Users/diogo/CLionProjects/-fsoft2026_1DA_5-/ProjectTester/Data - Testers/cmake-build-debug/googletest-main
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ ProjectTester/Data - Testers/cmake-build-debug/googletest-main/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/diogo/CLionProjects/-fsoft2026_1DA_5-/ProjectTester/Data - Testers/googletest-main/googletest
+# Build directory: C:/Users/diogo/CLionProjects/-fsoft2026_1DA_5-/ProjectTester/Data - Testers/cmake-build-debug/googletest-main/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
