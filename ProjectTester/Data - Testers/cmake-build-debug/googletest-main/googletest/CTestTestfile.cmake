227704c498f609bec68e5d2d50d966d8ffd2e4c2
Manuel Torre <1251209@isep.ipp.pt>
2026-06-13 19:20:56 +0100

    Delete ProjectTester/Data - Testers/cmake-build-debug directory

--- ProjectTester/Data - Testers/cmake-build-debug/googletest-main/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/diogo/CLionProjects/-fsoft2026_1DA_5-/ProjectTester/Data - Testers/googletest-main/googletest
-# Build directory: C:/Users/diogo/CLionProjects/-fsoft2026_1DA_5-/ProjectTester/Data - Testers/cmake-build-debug/googletest-main/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
